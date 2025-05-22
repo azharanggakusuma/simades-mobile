3dce7e284fd0a877225beecfbb1fbbb799241221
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-22 09:41:49 +0700

    fix: update tailwind config for consistent file patterns and clean up theme extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
   ],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins-Regular'],
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
