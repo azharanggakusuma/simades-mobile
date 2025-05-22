71216f2c82018bc231b0ca6e5e3e77e96b685620
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-22 10:27:14 +0700

    feat: refactor Navbar component to use Tailwind CSS for styling and remove unused styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
@@ -7,7 +7,10 @@ export default function Navbar({ onMenuPress, onProfilePress, isSidebarOpen }) {
   const insets = useSafeAreaInsets();
 
   return (
-    <View style={[styles.container, { paddingTop: insets.top + 10 }]}>
+    <View
+      style={{ paddingTop: insets.top + 10 }}
+      className="bg-white flex-row items-center px-4 pb-3 border-b border-[#e5e7eb] justify-between"
+    >
       <TouchableOpacity onPress={onMenuPress}>
         <Ionicons
           name={isSidebarOpen ? 'close' : 'menu'}
@@ -16,39 +19,13 @@ export default function Navbar({ onMenuPress, onProfilePress, isSidebarOpen }) {
         />
       </TouchableOpacity>
 
-      <Text style={styles.title}>SIMADES</Text>
+      <Text className="text-[18px] font-semibold text-[#111827]">SIMADES</Text>
 
-      <View style={styles.rightIcons}>
-        <TouchableOpacity onPress={onProfilePress} style={styles.iconButton}>
+      <View className="flex-row space-x-[12px]">
+        <TouchableOpacity onPress={onProfilePress} className="p-1">
           <Ionicons name="person-circle-outline" size={24} color="#374151" />
         </TouchableOpacity>
       </View>
     </View>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-    paddingBottom: 12,
-    borderBottomColor: '#e5e7eb',
-    borderBottomWidth: 1,
-    justifyContent: 'space-between',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#111827',
-  },
-  rightIcons: {
-    flexDirection: 'row',
-    gap: 12,
-  },
-  iconButton: {
-    padding: 4,
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
   ],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins-Regular", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
